786b68adb31ea6eed7f79044b48a09aad17ac1fb
Matt Van Dyke <matthewlvandyke@gmail.com>
2020-04-04 18:32:18 -0400

    Add listeners to startGame

--- startGame.js
+++ startGame.js
@@ -10,6 +10,22 @@ let dumbGlobalStartVariable = false;
 
 let cardsMap = null;
 
+const cardOrder = {
+  'A': 0,
+  '2': 1,
+  '3': 2,
+  '4': 3,
+  '5': 4,
+  '6': 5,
+  '7': 6,
+  '8': 7,
+  '9': 8,
+  '10': 9,
+  'J': 10,
+  'Q': 11,
+  'K': 12,
+};
+
 module.exports = (bot) => {
   bot.on('message', (user, userId, channelId, message, event) => {
     if (message === '!startKings') {
@@ -47,6 +63,72 @@ module.exports = (bot) => {
       }
 
       pickCard(bot, localCardsCopy, cardsMap, channelId);
+    }
+  });
+
+  bot.on('message', (user, userId, channelId, message, event) => {
+    if (message === '!help') {
+      const reply = [
+        'Meta commands:',
+        'Type `!startKings` to start a game',
+        'Type `!restartKings` to start a new game',
+        'Type `!kingsCardList` to see cards and rules',
+      ].join('\n');
+
+      bot.sendMessage({
+        to: channelId,
+        message: reply,
+      });
+    }
+  });
+
+  bot.on('message', (user, userId, channelId, message, event) => {
+    if (message === '!kingsCardList') {
+      const reply = [
+        'Rules vary for better remote play (use command with `-verbose` flag for more info):',
+        '`A: Waterfall`',
+        '`3: Me`',
+        '`4: Lore`',
+        '`5: Guys`',
+        '`6: Chicks`',
+        '`7: Thief`',
+        '`8: Mate`',
+        '`9: Fine`',
+        '`10: Category`',
+        '`J: Jinx`',
+        '`Q: Comrade Questions`',
+        '`K: King\'s Decree`',
+      ].join('\n');
+
+      bot.sendMessage({
+        to: channelId,
+        message: reply,
+      });
+    }
+
+    if (message === '!kingsCardList -verbose') {
+      const cardsMap = require('./getCardsMap')({ waterFallTime: 5 })
+
+      const reply = Object.keys(cardsMap)
+        .map((card) => {
+          const {gameType} = cardsMap[card];
+
+          return `\`${ card }:\` ${ gameType }`;
+        })
+        .sort(
+          (
+            [backtick1, a, zero],
+            [backtick2, b, zero2]) => (
+              (zero !== ':' ? cardOrder[a + zero] : cardOrder[a])
+                -
+              (zero2 !== ':' ? cardOrder[a + zero2] : cardOrder[b]))
+            )
+        .join('\n');
+
+      bot.sendMessage({
+        to: channelId,
+        message: 'Verbose descriptions...\n' + reply,
+      });
     }
   });
 
